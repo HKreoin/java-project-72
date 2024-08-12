4473b2734d5dbe0168441c1be4885968008d5f7b
Kreoin <steamuser85@yandex.ru>
2024-08-12 15:06:22 +0300

    add App and BaseRepository classes

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,49 @@
+package hexlet.code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import hexlet.code.repository.BaseRepository;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static Javalin getApp() throws IOException, SQLException {
+
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:h2:mem:hexlet;DB_CLOSE_DELAY=-1;");
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        var url = App.class.getClassLoader().getResourceAsStream("schema.sql");
+        var sql = new BufferedReader(new InputStreamReader(url))
+            .lines().collect(Collectors.joining("\n"));
+
+        log.info(sql);
+        try (var connection = dataSource.getConnection();
+                var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        return app;
+    }
+    public static void main(String[] args) throws IOException, SQLException {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
