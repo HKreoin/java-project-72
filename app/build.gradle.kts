766be80973c22918be3c0158cec2d624814179ff
Kreoin <steamuser85@yandex.ru>
2024-09-05 09:45:36 +0300

    add assertj, javalin-bundle into build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,10 +22,12 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.25.3")
 
     implementation("io.javalin:javalin:6.1.3")
-    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("com.h2database:h2:2.2.220")
     implementation("com.zaxxer:HikariCP:5.0.1")
