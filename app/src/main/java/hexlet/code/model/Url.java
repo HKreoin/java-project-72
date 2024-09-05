43191d9debf7072fe5c555d265706881546109ad
Kreoin <steamuser85@yandex.ru>
2024-09-05 15:54:38 +0300

    add getter and setter to Url entity

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -2,6 +2,11 @@ package hexlet.code.model;
 
 import java.time.LocalDateTime;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Url {
     private long id;
     private String name;
