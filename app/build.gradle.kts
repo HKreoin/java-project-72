5870bd26fd4b0634935dc7adb8167ede96ccaa00
Kreoin <steamuser85@yandex.ru>
2024-09-05 14:23:20 +0300

    update jte up to 3.1.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation("io.javalin:javalin:6.1.3")
     implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("io.javalin:javalin-rendering:6.1.3")
-    implementation("gg.jte:jte:3.1.9")
+    implementation("gg.jte:jte:3.1.12")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("com.h2database:h2:2.2.220")
     implementation("com.zaxxer:HikariCP:5.0.1")
