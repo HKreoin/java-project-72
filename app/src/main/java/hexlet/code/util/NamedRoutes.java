59b3d4c81b2800e81f6b2a598b0a9afec2288def
Kreoin <steamuser85@yandex.ru>
2024-09-06 11:36:48 +0300

    add routes

--- app/src/main/java/hexlet/code/util/NamedRoutes.java
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -2,11 +2,19 @@ package hexlet.code.util;
 
 public class NamedRoutes {
 
-    public static String index() {
+    public static String rootPath() {
         return "/";
     }
 
     public static String urlsPath() {
-        return "urls";
+        return "/urls";
+    }
+
+    public static String urlPath(long id) {
+        return urlPath(String.valueOf(id));
+    }
+
+    public static String urlPath(String id) {
+        return "/urls/" + id;
     }
 }
