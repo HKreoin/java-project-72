2a0b2bcc2eb6b940eb5ee13f3ce1240702b20923
Kreoin <steamuser85@yandex.ru>
2024-09-06 11:38:05 +0300

    add  constructor and getCreatedAtString method

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -1,6 +1,7 @@
 package hexlet.code.model;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,4 +12,13 @@ public class Url {
     private long id;
     private String name;
     private LocalDateTime createdAt;
+
+    public Url(String name) {
+        this.name = name;
+    }
+
+    public final String getCreatedAtString() {
+        var formatter = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm-ss");
+        return createdAt.format(formatter);
+    }
 }
