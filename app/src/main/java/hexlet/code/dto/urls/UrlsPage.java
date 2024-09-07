3f0b3e0b247c408d4648307e8a5f6c3dacad3813
Kreoin <steamuser85@yandex.ru>
2024-09-07 18:57:55 +0300

    add urlChecksMap

--- app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
@@ -1,10 +1,11 @@
 package hexlet.code.dto.urls;
 
+import java.util.Map;
 import java.util.List;
 
 import hexlet.code.dto.BasePage;
 import hexlet.code.model.Url;
-
+import hexlet.code.model.UrlCheck;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,14 +13,12 @@ import lombok.Setter;
 @Getter
 public class UrlsPage extends BasePage {
     private List<Url> urls;
+    private Map<Long, UrlCheck> urlChecksMap;
 
-    public UrlsPage(List<Url> urls) {
+    public UrlsPage(List<Url> urls, Map<Long, UrlCheck> urlChecksMap, String flashType, String flashMessage) {
         this.urls = urls;
-    }
-
-    public UrlsPage(List<Url> urls, String flashType, String flashMessage) {
-        this.urls = urls;
+        this.urlChecksMap = urlChecksMap;
         setFlashType(flashType);
-        setFlashType(flashMessage);
+        setFlashMessage(flashMessage);
     }
 }
