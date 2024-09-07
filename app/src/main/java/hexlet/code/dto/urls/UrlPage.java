3e6046466ccb5aa0250d8778b054faf58ed454dd
Kreoin <steamuser85@yandex.ru>
2024-09-07 18:57:22 +0300

    add checks list

--- app/src/main/java/hexlet/code/dto/urls/UrlPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlPage.java
@@ -1,8 +1,10 @@
 package hexlet.code.dto.urls;
 
+import java.util.List;
+
 import hexlet.code.dto.BasePage;
 import hexlet.code.model.Url;
-
+import hexlet.code.model.UrlCheck;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +12,10 @@ import lombok.Setter;
 @Getter
 public class UrlPage extends BasePage {
     private Url url;
+    private List<UrlCheck> checks;
 
-    public UrlPage(Url url) {
+    public UrlPage(Url url, List<UrlCheck> checks) {
         this.url = url;
+        this.checks = checks;
     }
 }
