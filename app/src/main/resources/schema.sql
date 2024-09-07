017664b75cf24f439a423fd94813e12c6c03e004
Kreoin <steamuser85@yandex.ru>
2024-09-07 18:41:36 +0300

    add url_cheks sql schema

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,19 @@
+DROP TABLE IF EXISTS url_checks;
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     created_at TIMESTAMP
 );
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    status_code INTEGER,
+    title VARCHAR(256),
+    h1 VARCHAR(256),
+    description TEXT,
+    url_id BIGINT REFERENCES urls (id),
+    created_at TIMESTAMP
+);
+
