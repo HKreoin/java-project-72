f119bc9780f1af6f8e1b2793efe2c9b6fc60b401
Kreoin <steamuser85@yandex.ru>
2024-09-07 18:41:16 +0300

    expansion tests

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,28 +1,69 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
+import hexlet.code.repository.UrlCheckRepository;
 import hexlet.code.repository.UrlRepository;
+import hexlet.code.util.NamedRoutes;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
 
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 
 public class AppTest {
 
-    Javalin app;
+    private static Javalin app;
+    private static MockWebServer mockWebServer;
+
+    private static final String TEST_PAGE = "TestPage.html";
+
+    private static Path getFixturePath(String fileName) {
+        return Paths.get("src", "test", "resources", "fixtures", fileName)
+                .toAbsolutePath().normalize();
+    }
+
+    private static String readFixture(String fileName) throws Exception {
+        Path filePath = getFixturePath(fileName);
+        return Files.readString(filePath).trim();
+    }
+
+    @BeforeAll
+    static void generalSetUp() throws Exception {
+        mockWebServer = new MockWebServer();
+        MockResponse mockResponse = new MockResponse()
+                .setBody(readFixture(TEST_PAGE))
+                .setResponseCode(200);
+        mockWebServer.enqueue(mockResponse);
+        mockWebServer.start();
+    }
 
     @BeforeEach
-    public final void setUp() throws IOException, SQLException {
+    public final void setUpForEachTest() throws IOException, SQLException {
         app = App.getApp();
     }
 
+    @AfterAll
+    static void tearDown() throws IOException {
+        mockWebServer.shutdown();
+        app.stop();
+    }
     @Test
     public void testMainPage() {
         JavalinTest.test(app, (server, client) -> {
@@ -55,6 +96,29 @@ public class AppTest {
         JavalinTest.test(app, (server, client) -> {
             var response = client.get("/urls/1234567890");
             assertThat(response.code()).isEqualTo(404);
+        });
+    }
+
+    @Test
+    public void testCheckUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            String mockUrlName = mockWebServer.url("/").toString();
+            Url mockUrl = new Url(mockUrlName);
+            UrlRepository.save(mockUrl);
+
+            var response = client.post(NamedRoutes.urlCheck(String.valueOf(mockUrl.getId())));
+            assertThat(response.code()).isEqualTo(200);
+
+            List<UrlCheck> urlChecks = UrlCheckRepository.findUrlChecks(mockUrl.getId());
+            assertThat(urlChecks.size()).isEqualTo(1);
+
+            UrlCheck lastUrlCheck = UrlCheckRepository.findUrlChecks(mockUrl.getId()).getFirst();
+            assertThat(lastUrlCheck.getUrlId()).isEqualTo(1);
+            assertThat(lastUrlCheck.getStatusCode()).isEqualTo(200);
+            assertThat(Timestamp.valueOf(lastUrlCheck.getCreatedAt())).isToday();
+            assertThat(lastUrlCheck.getTitle()).contains("Test page");
+            assertThat(lastUrlCheck.getH1()).contains("Test page h1");
+            assertThat(lastUrlCheck.getDescription()).contains("Test description");
         });
     }
 }
