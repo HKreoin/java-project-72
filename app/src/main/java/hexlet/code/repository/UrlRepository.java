05fa1d27fe52e5841b62e093edeac218dfc3647a
Kreoin <steamuser85@yandex.ru>
2024-09-08 17:26:43 +0300

    add find by name method

--- app/src/main/java/hexlet/code/repository/UrlRepository.java
+++ app/src/main/java/hexlet/code/repository/UrlRepository.java
@@ -15,13 +15,13 @@ public class UrlRepository extends BaseRepository {
     public static void save(Url url) throws SQLException {
         var sql = "INSERT INTO urls (name, created_at) VALUES (?, ?)";
         try (var conn = dataSource.getConnection();
-                var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            preparedStatement.setString(1, url.getName());
+                var stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, url.getName());
             var createdAt = LocalDateTime.now();
-            preparedStatement.setTimestamp(2, Timestamp.valueOf(createdAt));
+            stmt.setTimestamp(2, Timestamp.valueOf(createdAt));
 
-            preparedStatement.executeUpdate();
-            var generatedKeys = preparedStatement.getGeneratedKeys();
+            stmt.executeUpdate();
+            var generatedKeys = stmt.getGeneratedKeys();
             if (generatedKeys.next()) {
                 url.setId(generatedKeys.getLong(1));
                 url.setCreatedAt(createdAt);
@@ -58,6 +58,27 @@ public class UrlRepository extends BaseRepository {
             return resultSet.next();
         }
     }
+
+
+    public static Optional<Url> findByName(String urlName) throws SQLException {
+        String sql = "SELECT * FROM urls WHERE name = ?";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, urlName);
+            var resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                String name = resultSet.getString("name");
+                LocalDateTime dateandtime = resultSet.getObject("created_at", LocalDateTime.class);
+                long id = resultSet.getLong("id");
+                Url url = new Url(name);
+                url.setId(id);
+                url.setCreatedAt(dateandtime);
+                return Optional.of(url);
+            }
+            return Optional.empty();
+        }
+    }
+
 
     public static List<Url> getEntities() throws SQLException {
         var sql = "SELECT * FROM urls";
